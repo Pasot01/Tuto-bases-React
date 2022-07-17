4680cc2ce45a4be1ed2c949e157301fdf16d52a5
Pasot01 <contactmyphone14@gmail.com>
2022-07-17 22:16:17 +0200

    Mise en place de 4 composants sur App.js avant routage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/app.css';
+import Home from './pages/Home'
+import Menu from './components/Menu';
+import TechnoAdd from './pages/TechnoAdd';
+import TechnoList from './pages/TechnoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Menu />
+      <Home />
+      <TechnoAdd />
+      <TechnoList />
+    </>
   );
 }
+
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h1>My progression</h1>
+            <div>Welcome to the site devoted to your programming progression</div>
+        </div>
+    )
+}
